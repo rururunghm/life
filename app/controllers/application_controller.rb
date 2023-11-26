14fadc191587d8c32fa99553737915c407b095b9
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-11-26 08:42:43 +0000

    update/検索時の削除ボタン訂正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  
+ 
   protected
 
   def configure_permitted_parameters
--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -1,4 +1,5 @@
 class User::HomesController < ApplicationController
+  before_action :authenticate_user!
   
   def top
   end
